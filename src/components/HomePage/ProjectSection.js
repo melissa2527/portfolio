d149c4f91e89f6f82ab7b3d0489461900b701441
annie2527 <you@example.com>
2021-03-02 11:45:31 +0100

    project section

--- src/components/HomePage/ProjectSection.js
+++ src/components/HomePage/ProjectSection.js
@@ -15,24 +15,28 @@ export const ProjectSection = () => {
 
     return (
         <div className='project-container'>
-            <button onClick={prevPicture} className='left-arrow'><i className="fas fa-arrow-left"></i></button>
-            <button onClick={nextPicture} className='right-arrow'><i className="fas fa-arrow-right"></i></button>
+            <h2>Projects</h2>
+            <p className='project-info'>Check out more on the <a href='/projects'>Projects Page</a> or GitHub <a href='https://github.com/melissa2527' target='_blank' rel='noreferrer'>here</a></p>
             {projectListData.map((image, index) => {
                 return (
                     <div className={index === current ? 'slide-active' : 'slide'} key={index}>
                         {index === current && (
                             <div>
                             <img src={image.image} alt='' className='image' height={253} width={450}/>
-                            <p>{image.name}</p>
+                            <p className='title'>{image.title}</p>
                             </div>
-                )}
-                </div>
-                )
+                        )}
+                    </div>
+                    )
                         })}
-
             <div className='project--section'>
                 <img src='./img/computer.png' alt='laptop'/>    
             </div>
+            <div className='project--section-btns'>
+                <button onClick={prevPicture} className='left-arrow'><i className="fas fa-arrow-left"></i></button>
+                <button onClick={nextPicture} className='right-arrow'><i className="fas fa-arrow-right"></i></button>
+            </div>
+        
         </div>
     )
 }
