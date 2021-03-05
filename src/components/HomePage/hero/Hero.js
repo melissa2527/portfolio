5aeaf3123f4fbf7ec178a973a9a87ca6dbd33986
annie2527 <you@example.com>
2021-03-05 14:10:18 +0100

    fix hero button

--- src/components/HomePage/hero/Hero.js
+++ src/components/HomePage/hero/Hero.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import {Button} from '../../Button';
+import {Link} from 'react-router-dom';
 
 export const Hero = () => {
     const [isShown, setIsShown] = useState(true);
@@ -25,7 +26,11 @@ export const Hero = () => {
                         buttonStyle='btn--outline'
                         buttonSize='btn--lge'
                     >
-                        {isShown ? 'SEE MORE' : 'SCROLL DOWN'}
+                        <Link to='/projects' id='hero-btn'
+                        // style={{'text-decoration': 'none', 'color': 'whitesmoke'}}
+                        >
+                            {isShown ? 'SEE MORE' : 'SCROLL DOWN'}
+                        </Link>
                     </Button>
                 </div>
             </div>
