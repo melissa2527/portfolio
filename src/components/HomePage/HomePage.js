74d1c9a91353f1332d98f604cd81117f23960505
annie2527 <you@example.com>
2021-03-06 15:56:44 +0100

    media

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,19 +1,13 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Hero} from './hero/Hero';
-import AOS from 'aos';
 import './homepage.scss';
 import { ProjectSection } from './projectsection/ProjectSection';
 import { TechStack } from './tech/TechStack';
 import { AboutSection } from './about/AboutSection';
 
 export const HomePage = () => {
-
-    useEffect(() => {
-        AOS.init({duration: 1500})
-    })
-
     return (
-        <div>
+        <div style={{'overflow-x': 'hidden'}}>
             <Hero />
             <AboutSection/>
             <TechStack />
