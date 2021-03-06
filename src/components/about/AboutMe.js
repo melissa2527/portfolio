27e7e1459ec31e200a820822d006540a9d146782
annie2527 <you@example.com>
2021-03-06 22:10:47 +0100

    cards

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -4,7 +4,7 @@ import './aboutme.scss';
 
 export const AboutMe = () => {
     return (
-        <div className='aboutme-container'>
+        <div className='aboutme-container' style={{'overflowX': 'hidden'}}>
             <AboutSection/>
             <p data-aos='fade-down' className='aboutme-info'>Find me on <a href='https://www.linkedin.com/in/melissa-anne-genger/' target='_blank' rel='noreferrer'>LinkedIn</a></p>
         </div>
